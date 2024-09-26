04c27481db2eba38587774b28df8dee2fb9fad51
JohnSovero <u202115065@upc.edu.pe>
2024-09-25 21:55:32 -0500

    feat: Interfaz

--- src/app/main.go
+++ src/app/main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-
 	dnn := deep.NeuralNetwork{}
 	dnn.Initialize([]int{5, 10, 5, 1}, 0.0001)
 
@@ -28,11 +27,20 @@ func main() {
 	// Console for input data from the user
 	var input []float64
 	columns := df.Headers
-	i := 1
+	it := 1
 	for {
-		fmt.Printf("\nPrediccion %d : \n", i)
+		fmt.Printf("\nPrediccion %d : \n", it)
 		for i := 0; i < len(columns)-1; i++ {
-			fmt.Printf("Ingrese el valor de %s \n", columns[i])
+			if i == 0 {
+				fmt.Printf("Ingrese el valor de %s (0: No declaró importe, 1: Sí declaró importe) \n", columns[i])
+			} else if i == 2 {
+				fmt.Printf("Ingrese el valor de %s (0: Privado, 1: Público) \n", columns[i])
+			} else if i == 4 {
+				fmt.Printf("Ingrese el valor de %s (número de meses) \n", columns[i])
+			} else {
+				fmt.Printf("Ingrese el valor de %s \n", columns[i])
+			}
+			
 			var value float64
 			fmt.Scan(&value)
 			input = append(input, value)
@@ -42,7 +50,6 @@ func main() {
 		fmt.Printf("\nRemuneracion Esperada: S/. %.2f\n", predict[0])
 		// clean input
 		input = nil
-		i++
+		it++
 	}
-
 }
